780bee671229228f925acd53caba7d8e93142c28
OukhellouFst <oukhellou.fst@gmail.com>
2023-02-14 08:00:24 +0100

    editing of the main function of our app so the user tap the class name*

--- src/strategy/App.java
+++ src/strategy/App.java
@@ -1,12 +1,27 @@
 package strategy;
-import strategy.Interface_Implementations.StrategyImpl1;
+import java.util.Scanner;
+import java.lang.Class;
+
+import strategy.Interface_Implementations.*;
+
 
 public class App {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception{
 		Context context = new Context();
-		context.setStrategy(new StrategyImpl1());
-		context.process();
+		
+		boolean fin = false;
+		Scanner scanner = new Scanner(System.in);
+		while(!fin ) {
+			System.out.println("Donner la strategie");
+			String strategyClassName = scanner.nextLine();
+			/* il faut donner le nom complet de la classe 
+			 * exp: strategy.Interface_Implementations.StrategyImpl1
+			 */
+			IStrategy strategy = (IStrategy) Class.forName(strategyClassName).newInstance();
+			context.setStrategy(strategy);
+			context.process();
+		}
 	}
 
 }
